dcd998010bed65d746fbd976bbc76e29703c1bcd
hokeys <substantiation@naver.com>
2019-06-28 22:13:51 +0900

    작동함

---
+++ config-overrides.js
@@ -0,0 +1,7 @@
+
+module.exports = function override(config, env) {
+  config = rewireProvidePlugin(config, env, {
+    'window.Quill': ['react-quill', 'Quill']
+  })
+  return config;
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,97 @@
-import React from 'react';
+import ReactQuill from 'react-quill';
+import React from 'react'
+import PropTypes from 'prop-types'
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+/* 
+ * Simple editor component that takes placeholder text as a prop 
+ */
+class Editor extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = { editorHtml: '', theme: 'snow' }
+    this.handleChange = this.handleChange.bind(this)
+  }
+  
+  handleChange (html) {
+  	this.setState({ editorHtml: html });
+  }
+  
+  handleThemeChange (newTheme) {
+    if (newTheme === "core") newTheme = null;
+    this.setState({ theme: newTheme })
+  }
+  
+  render () {
+    return (
+      <div>
+        <ReactQuill 
+          theme={this.state.theme}
+          onChange={this.handleChange}
+          value={this.state.editorHtml}
+          modules={Editor.modules}
+          formats={Editor.formats}
+          bounds={'.app'}
+          placeholder={this.props.placeholder}
+         />
+        <div className="themeSwitcher">
+          <label>Theme </label>
+          <select onChange={(e) => 
+              this.handleThemeChange(e.target.value)}>
+            <option value="snow">Snow</option>
+            <option value="bubble">Bubble</option>
+            <option value="core">Core</option>
+          </select>
+        </div>
+       </div>
+     )
+  }
+}
+
+/* 
+ * Quill modules to attach to editor
+ * See https://quilljs.com/docs/modules/ for complete options
+ */
+Editor.modules = {
+  toolbar: [
+    [{ 'header': '1'}, {'header': '2'}, { 'font': [] }],
+    [{size: []}],
+    ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+    [{'list': 'ordered'}, {'list': 'bullet'}, 
+     {'indent': '-1'}, {'indent': '+1'}],
+    ['link', 'image', 'video'],
+    ['clean']
+  ],
+  clipboard: {
+    // toggle to add extra line breaks when pasting HTML:
+    matchVisual: false,
+  }
+}
+/* 
+ * Quill editor formats
+ * See https://quilljs.com/docs/formats/
+ */
+Editor.formats = [
+  'header', 'font', 'size',
+  'bold', 'italic', 'underline', 'strike', 'blockquote',
+  'list', 'bullet', 'indent',
+  'link', 'image', 'video'
+]
+
+/* 
+ * PropType validation
+ */
+Editor.propTypes = {
+  placeholder: PropTypes.string,
+}
+
+/* 
+ * Render component on page
+ */
+ReactDOM.render(
+  <Editor placeholder={'Write something...'}/>, 
+  document.querySelector('.app')
+)
